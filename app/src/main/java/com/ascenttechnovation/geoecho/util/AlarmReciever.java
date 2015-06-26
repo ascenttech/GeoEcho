c276ab33c695a3687a0773172e61f7aa92bc63f9
Ascenttech <tech@ascentinc.in>
2015-06-26 11:48:02 +0530

    Shared pref cleared

--- app/src/main/java/com/ascenttechnovation/geoecho/util/AlarmReciever.java
+++ app/src/main/java/com/ascenttechnovation/geoecho/util/AlarmReciever.java
@@ -3,20 +3,26 @@ package com.ascenttechnovation.geoecho.util;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 /**
  * Created by ADMIN on 25-06-2015.
  */
 public class AlarmReciever extends BroadcastReceiver {
 
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
     @Override
     public void onReceive(Context context, Intent intent) {
 
               // WRITE THE CODE FOR CLEARING THE SHARED PREFERENCE
-
-                // PROVIDE USER LOGIN
-
-            // clear the shared preference
+        pref = context.getSharedPreferences("myPref",0);
+        editor=pref.edit();
+        editor.remove("contactNo");
+        editor.remove("login");
+        editor.clear();
+        editor.commit();
+            // cleared the shared preference
 
 
     }
